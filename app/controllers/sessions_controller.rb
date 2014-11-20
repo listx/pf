ac166fac1e4fd4ecf7c9fa34cde206f3506f0813
Linus Arver <linusarver@gmail.com>
2014-11-19 17:52:44 -0800

    add user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    # User model's 'has_secure_password' gives us the '#authenticate' method.
+    if !user.nil? && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to controller: :dashboard, action: :index
+    else
+      flash.now[:danger] = "Invalid username/password combination."
+      render 'new'
+    end
+  end
 end
