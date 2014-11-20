96c3a95e1932454aa177e94132a1c2348bc67856
Linus Arver <linusarver@gmail.com>
2014-11-19 17:40:39 -0800

    use bcrypt and configure User model for mongodb
    In particular, we cannot use the "Digest" type because mongodb does not
    have that as a native type; instead, we use the String type, and rename
    the field to "password_digest".

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User
   include Mongoid::Document
+
+  # MongoDB/Mongoid requires this SecurePassword import for
+  # 'has_secure_password' to work.
+  include ActiveModel::SecurePassword
+
   field :name, type: String
-  field :password, type: Digest
+  field :password_digest, type: String
+  has_secure_password
 end
