5d71e76443fca3e45daf60027e03adf87e9c2e37
Linus Arver <linusarver@gmail.com>
2014-11-24 17:40:00 -0800

    attachment: validate binary types key

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,5 @@
+require_relative 'validators.rb'
+
 class Attachment
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -6,4 +8,7 @@ class Attachment
 
   field :name, type: String
   field :content, type: BSON::Binary
+  field :content_type, type: Integer
+
+  validates_with ValidateBinaryTypesRange, binary_types_key: :content_type
 end
