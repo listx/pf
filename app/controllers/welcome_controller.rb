8b7ccff9a316094a568994cd7c1bb7a7ed58819d
Linus Arver <linusarver@gmail.com>
2014-12-09 23:05:12 -0800

    hard-code a special journal for the landing page blurb
    The name of the journal must be "*pf-landing-page*".

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
+  include JournalHelper
   def index
+    @journal_special = Journal.find_by(name: "*pf-landing-page*")
+    @journal_special_render = gen_render(@journal_special.natree)
     @jtotal = Journal.all.size
     @journals = Journal.where(\
       user_id: User.find_by(name: "root").id, published: true)\
